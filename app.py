9bd51bd99fa4310d73f04a7daaeb0e45101a2bd9
imvishnu92 <56810955+imvishnu92@users.noreply.github.com>
2024-12-17 06:29:52 +0000

    Added dynamic data and contact us pop up

--- app.py
+++ app.py
@@ -1,7 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 app = Flask(__name__)
+
+PRODUCTS = [
+    {'id':1,
+    'title':'Solder Preforms',
+    'description':'Solder Preforms'},
+    {'id':2,
+    'title':'Solder Pins',
+    'description':'Solder Pins'},
+    {'id':3,
+    'title':'Solder Strips',
+    'description':'Solder Strips'},
+    {'id':4,
+    'title':'Solder Jacks',
+    'description': 'Solder Jacks'}
+]
+
 @app.route('/')
 def hello_sriram():
-    return render_template('home.html')
+    return render_template('home.html', products=PRODUCTS)
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
